49c315e6bfd505c313c128d75df52ba1873e8d9e
Simon Moe <simmoe@gmail.com>
2019-10-22 11:04:47 +0200

    push with gh pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   return(
-    <h1>this stuff works</h1>
+    <div className="app">
+      <h1>App.js</h1>
+      <p>App.js is the parent file in a hierarchy of submodules we will create.</p>
+      <div className="submodules">
+        <h2>no submodules yet</h2>
+      </div>
+    </div>
   )
 }
 
 export default App
+
+/* 
+GITHUB PAGES DEPLOY STEPS 
+
+Push repo to github and create a github pages branch from master
+
+Copy the URL of the branch - https://simmoe.github.io/react-course/
+
+Install github-pages node module from the folder terminal: npm install gh-pages --save 
+
+Locate the package.json file in your root directory, add this line of code to the top of the object: "homepage": "link-to-your-repository", and save.
+
+In your package.json file, locate “scripts” add these lines of code:
+{
+   ...
+   "predeploy": "yarn run build",
+   "deploy": "gh-pages -d build",
+   ...
+}
+
+*/
 
