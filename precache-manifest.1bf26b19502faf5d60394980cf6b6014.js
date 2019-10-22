c8848e723d6f6c669d367d85c404d6ae94f295a2
Simon Moe <simmoe@gmail.com>
2019-10-22 11:04:03 +0200

    Updates

---
+++ precache-manifest.1bf26b19502faf5d60394980cf6b6014.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698de2f9006cb88b57053aec6490b8ef",
+    "url": "/react-course/index.html"
+  },
+  {
+    "revision": "37644cf10b3b7213b6a6",
+    "url": "/react-course/static/css/main.34ba1254.chunk.css"
+  },
+  {
+    "revision": "be1ce0291752221c5641",
+    "url": "/react-course/static/js/2.1823a9d0.chunk.js"
+  },
+  {
+    "revision": "37644cf10b3b7213b6a6",
+    "url": "/react-course/static/js/main.401c5827.chunk.js"
+  },
+  {
+    "revision": "59aba37ea228d9cbeb36",
+    "url": "/react-course/static/js/runtime-main.92452752.js"
+  }
+]);
